0fec564fc2893597ee1f5bf188855d207b1dcad3
Mucyo Miller <mucyomiller@gmail.com>
2020-06-14 19:52:01 +0200

    [WIP] adds payment screen UI

--- lib/src/pages/payment_page.dart
+++ lib/src/pages/payment_page.dart
@@ -1,10 +1,227 @@
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class PaymentPage extends StatelessWidget {
+class PaymentPage extends StatefulWidget {
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  TextEditingController _phoneTextEditController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('payment page!'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Payment',
+          style: TextStyle(color: Colors.white),
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 40.0,
+                        backgroundImage:
+                            AssetImage('assets/default_avatar.png'),
+                        backgroundColor: Colors.transparent,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'Rindiro',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text('Parent')
+                        ],
+                      ),
+                      Spacer(),
+                      Text(
+                        'updated on 12/08',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      )
+                    ],
+                  ),
+                  Text(
+                    'Payment duration',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Divider(),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'From',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text('12/9/2018'),
+                        ],
+                      ),
+                      Spacer(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'To',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text('12/10/2018')
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                      'This is a payment section, you can upgrade you children subscription by providing the information below.'),
+                  Text(
+                    'PAYMENT FORM',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Divider(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Theme.of(context).primaryColor, width: 1),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1),
+                      ),
+                      hintText: 'Total payment amount',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Theme.of(context).primaryColor, width: 1),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1),
+                      ),
+                      hintText: 'Names',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      CountryCodePicker(
+                        onChanged: print,
+                        initialSelection: 'RW',
+                        showFlagDialog: true,
+                        showOnlyCountryWhenClosed: false,
+                        alignLeft: false,
+                        textStyle: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 4.0),
+                          child: TextFormField(
+                            controller: _phoneTextEditController,
+                            keyboardType: TextInputType.number,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                            textInputAction: TextInputAction.done,
+                            validator: (v) =>
+                                v.isEmpty ? 'Phone number is required' : null,
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor,
+                                    width: 1),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey, width: 1),
+                              ),
+                              hintText: 'Names',
+                            ),
+                            inputFormatters: [
+                              new BlacklistingTextInputFormatter(
+                                new RegExp('[\\.|\\,|\\-|\\ ]'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlineButton(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(
+                          'CONFIRM',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      onPressed: () {},
+                      borderSide: BorderSide(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
